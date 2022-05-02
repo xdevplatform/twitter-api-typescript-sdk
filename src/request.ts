25d04a4024759475283999ea04dc604867851dc0
refarer <14077091+refarer@users.noreply.github.com>
2022-05-03 11:23:22 +1200

    bump version

--- src/request.ts
+++ src/request.ts
@@ -60,7 +60,7 @@ export async function request({
     url.toString(),
     {
       headers: {
-        "User-Agent": `twitter-api-typescript-sdk/1.0.4`,
+        "User-Agent": `twitter-api-typescript-sdk/1.0.5`,
         ...(isPost
           ? { "Content-Type": "application/json; charset=utf-8" }
           : undefined),
